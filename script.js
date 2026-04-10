6f3c12a43e1a7482f6e64f1716bf4591a019076f
navpreetchahal5504-hue <navpreetchahal5504@gmail.com>
2026-04-09 23:56:31 -0400

    Correct small mistakes

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function toggleDarkMode() {
 
 // Change Text
 function changeText() {
-    document.getElementById(aboutText).textContent =
+    document.getElementById("aboutText").textContent =
     "You clicked the button! I enjoy buildings websites,";
 }
 // Form Interaction
 function showName(event) {
     event.preventDefault();
 
-    let name = document.getElementById(nameInput).value;
+    let name = document.getElementById("nameInput").value;
 
     document.getElementById("output").textContent =
     "Thanks for your message"  + name + "!";
