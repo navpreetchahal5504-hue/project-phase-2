1e53f7b9791fe4c7e1d759b9eb1ee38f115a965a
navpreetchahal5504-hue <navpreetchahal5504@gmail.com>
2026-04-09 23:30:07 -0400

    Created a javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+// Dark Mode 
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+// Change Text
+function changeText() {
+    document.getElementById(aboutText).textContent =
+    "You clicked the button! I enjoy buildings websites,";
+}
+// Form Interaction
+function showName(event) {
+    event.preventDefault();
+
+    let name = document.getElementById(nameInput).value;
+
+    document.getElementById("output").textContent =
+    "Thanks for your message"  + name + "!";
+}
